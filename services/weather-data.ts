e38c46f943d83ec9ba2c5e4652865f59529ddb98
tiwariParth <243mjparth@gmail.com>
2024-11-14 07:16:20 +0530

    Update weather data service to use new API endpoint and headers

--- services/weather-data.ts
+++ services/weather-data.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const API_KEY = "a9b9dd1b31mshb69bed470c4ef7cp1b7d2fjsn84032a859d6d";
-const BASE_URL = "https://open-weather13.p.rapidapi.com";
+const BASE_URL = "https://open-weather13.p.rapidapi.com/city";
 
 export const fetchWeather = async (city: string) => {
-  const response = await axios.get(
-    `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
-  );
+  const response = await axios.get(`${BASE_URL}/${city}`, {
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
+    },
+  });
   return response.data;
 };
